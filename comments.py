c37ead51b31698c27ab3a5ae8343faf739cb92f1
Kevin <kevinarmero3396@gmail.com>
2025-08-23 12:32:09 -0500

    Add initial files for Travel Planner and welcome programs

---
+++ comments.py
@@ -0,0 +1,24 @@
+"""
+Program: Travel Planner
+Author: Brother Burton
+
+Description: Helps the user plan a vacation by computing
+    the costs associated with their travel.
+"""
+
+# display a greeting
+print("Welcome to the vacation planner program! ")
+
+# gather user input
+destination = input("Where would you like to go? ")
+# days should never be negative
+days = input("How many days will you vacation be? ")
+
+# ask for the budget for the food nd lodging
+
+# calculate the lodging cost, food cost, and the total price
+
+# display the results 
+
+# display the itinerary
+print(f"Itineray for the {days}-day trip to {destination}")
